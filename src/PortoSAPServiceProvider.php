d1adfb6434b1b9d2d6d7336832a89a19eef40bac
Rex Escario Jr <rex@neso.io>
2020-03-01 22:14:59 +0800

    initial commit

---
+++ src/PortoSAPServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Monkeykau\Porto;
+
+use Illuminate\Support\ServiceProvider;
+
+class PortoSAPServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__ . '/routes.php';
+    }
+}
